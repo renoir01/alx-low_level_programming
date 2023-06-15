7af16b3b178537bbdc970c6494b840fd95312bc1
renoir01 <r.kaze@alustudent.com>
2023-06-15 05:53:14 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+*/
+
+int main(void)
+{
+	char e;
+	int f;
+	long int b;
+	long long int c;
+	float d;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(e));
+	printf("Size of an int: %d byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a float: %d byte(s)\n", sizeof(d));
+
+	return (0);
+}
