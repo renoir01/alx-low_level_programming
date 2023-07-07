923fb7c487da25ea04bd5c9559bc07fe2d4cda58
renoir01 <r.kaze@alustudent.com>
2023-07-07 13:44:15 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
- * palind2 - obtains lenght of a
- * @a: string
- * @l: inter to count length
- *
- * Return: on succes 1.
- * On error, -1 is returned, and errno is set approximately
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
 
-int palind2(char *a, int l)
+int _strlen_recursion(char *s)
 {
-	if (*a == 0)
-		return (l - 1);
-	return (palind2(a + 1, l + 1));
-}
-
-/**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- *
- * Return: on success 1
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int palind3(char *a, int l)
-{
-	if (*a != *(a + l))
+	if (!*s)
+	{
 		return (0);
-	else if (*a == 0)
-		return (1);
-	return (palind3(a + 1, 1 - 2));
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
- *
- * Return: on sucess 1
- * On error, -1 is returned, and errno is set approproately
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
  */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
 int is_palindrome(char *s)
 {
-	int l;
+	int len = _strlen_recursion(s);
 
-	l = palind2(s, 0);
-	return (palind3(s, l));
+	return (p1(s, len - 1));
 }
