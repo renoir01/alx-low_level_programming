6b31989a5a562989300dca5b3014da08c7db2248
renoir01 <r.kaze@alustudent.com>
2023-07-13 14:07:21 -0700

    C is hard

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,72 +1,74 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
 
-/**
- *main - entry point to program that multiplies positive numbers
- *@argc: count of inputs
- *@argv: array of inputs
- *Return: 0 if success
- */
-int main(int argc, char **argv)
-{
-	if (argc > 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (all_digits(argv[1]) && all_digits(argv[2]))
-	{
-		print_number(atoll(argv[1]) * atoll(argv[2]));
-		_putchar('\n');
-	}
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	return (0);
+int multiply(const char *num1, const char *num2) {
+    int len1 = 0, len2 = 0, i, j;
+    int *result, carry, n1, n2, sum;
+
+    // Calculate the length of both numbers
+    while (num1[len1])
+        len1++;
+    while (num2[len2])
+        len2++;
+
+    // Allocate memory for the result
+    result = malloc(sizeof(int) * (len1 + len2));
+    if (result == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    // Initialize the result array with 0
+    for (i = 0; i < len1 + len2; i++)
+        result[i] = 0;
+
+    // Multiply each digit and store the result in the result array
+    for (i = len1 - 1; i >= 0; i--) {
+        n1 = num1[i] - '0';
+        carry = 0;
+
+        for (j = len2 - 1; j >= 0; j--) {
+            n2 = num2[j] - '0';
+            sum = n1 * n2 + result[i + j + 1] + carry;
+            result[i + j + 1] = sum % 10;
+            carry = sum / 10;
+        }
+
+        result[i] += carry;
+    }
+
+    // Find the first non-zero digit in the result array
+    i = 0;
+    while (result[i] == 0 && i < len1 + len2 - 1)
+        i++;
+
+    // Print the result
+    for (; i < len1 + len2; i++)
+        printf("%d", result[i]);
+    printf("\n");
+
+    free(result);
+    return 0;
 }
 
-/**
- *all_digits - checks an array to make sure all characters are digits
- *
- *@n: the array to be checked
- *Return: 0 if not all digits, and 1 if all digits
- */
-int all_digits(char *n)
-{
-	int i = 0;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Error: Invalid number of arguments\n");
+        return 98;
+    }
 
-	while (n[i] != '\0')
-	{
-		if (!isdigit(n[i]))
-		{
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
+    // Check if num1 and num2 are composed of digits
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                fprintf(stderr, "Error: Arguments must be composed of digits\n");
+                return 98;
+            }
+        }
+    }
 
-/**
- *print_number - prints numbers using putchar
- *@n: Number to be printed
- *
- *Return: void
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+    multiply(argv[1], argv[2]);
 
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar('0' + (n % 10));
+    return 0;
 }
